ddc9220a5db1ce077fc1e0416459ac72cb1fea98
欢 <whuihuan@outlook.com>
2016-08-03 01:04:16 -0700

    GTA3字库更变

--- wm_lcchs/WMLC.h
+++ wm_lcchs/WMLC.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 #include <windows.h>
 
-#define WMVERSIONWSTRING L"《侠盗猎车手3》汉化补丁 Satori(1.0) Build20160730 by 无名汉化组"
+#define WMVERSIONWSTRING L"《侠盗猎车手3》汉化补丁 Satori(1.0) Build20160803 by 无名汉化组"
 
 class WMLC
 {
